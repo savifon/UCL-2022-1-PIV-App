ebb21d360c9b5f870734a5c704415f398680778c
savifon <savifon1@gmail.com>
2022-03-21 17:39:42 -0300

    ajuste lista de produtos

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,26 @@
 <script setup>
 import ProductList from "../components/ProductList.vue";
-import products from '@/products.json';
+
+const products = [
+  {
+    "id": 1,
+    "image": "product-1.jpg",
+    "name": "Ração para cães adultos a granel - 100g",
+    "price": 123.45
+  },
+  {
+    "id": 2,
+    "image": "product-1.jpg",
+    "name": "Ração para gatos adultos a granel - 100g",
+    "price": 345.67
+  },
+  {
+    "id": 3,
+    "image": "product-1.jpg",
+    "name": "Ração para cães filhotes a granel - 100g",
+    "price": 567.89
+  }
+]
 </script>
 
 <template>
