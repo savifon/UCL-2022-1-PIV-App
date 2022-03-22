914bf51aa47da2141250dd084d5ae562e8612977
savifon <savifon1@gmail.com>
2022-03-22 00:29:38 -0300

    store products

--- src/stores/products.js
+++ src/stores/products.js
@@ -1,11 +1,30 @@
 import { defineStore } from "pinia";
 
+const productsStoreUrl =
+  "https://raw.githubusercontent.com/savifon/UCL-2022-1-PIV-App/master";
+
 export const useProductsStore = defineStore({
   id: "products",
 
-  state: () => {
-    items: [];
+  state: () => ({
+    items: {},
+    ids: []
+  }),
+
+  getters: {
+    list() {
+      return this.ids.map((i) => this.items[i]);
+    }
   },
 
-  actions: {}
+  actions: {
+    async fetchProducts() {
+      const res = await fetch(`${productsStoreUrl}/products.json`);
+      const data = await res.json();
+      this.ids = data.map((product) => {
+        this.items[product.id] = product;
+        return product.id;
+      });
+    }
+  }
 });
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,11 @@
 <script setup>
+import { computed } from "vue";
 import ProductList from "../components/ProductList.vue";
+import { useProductsStore } from '../stores/products';
+
+const productStore = useProductsStore();
+productStore.fetchProducts();
+const products = computed(() => productStore.list);
 </script>
 
 <template>
