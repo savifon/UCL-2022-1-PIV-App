28367277d5cd315556412340a7677090470c4ae9
Savio Fonseca <savifon1@gmail.com>
2022-03-23 14:27:52 -0300

    .

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,12 @@ productStore.fetchProducts();
 
 const cartStore = useCartStore();
 
-const currentUrlPath = computed(() => router.currentRoute.value.path);
-console.log(currentUrlPath)
+// const currentUrlPath = computed(() => router.currentRoute.value.path);
+// console.log(currentUrlPath)
 </script>
 
 <template>
-  <HeaderApp v-if="currentUrlPath !== '/'" :countCart="cartStore.count" />
+  <HeaderApp :countCart="cartStore.count" />
 
   <main class="main-app">
     <RouterView />
